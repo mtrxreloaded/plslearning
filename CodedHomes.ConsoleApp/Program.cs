916b04637dd3dab3d68bea435323f2dd56bc99da
Yifan Li <yifan.li@infocus.com>
2015-07-27 15:38:05 -0700

    Creating Models and the database

---
+++ CodedHomes.ConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using CodedHomes.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodedHomes.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Initializing Database...");
+
+            DataContext context = new DataContext();
+            context.Database.Initialize(true);
+
+            Console.WriteLine("Done");
+            Console.ReadLine();
+        }
+    }
+}
